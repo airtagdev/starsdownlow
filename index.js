1c2a49c23ab0f623d4347e1506d67df85c022bd0
airtag <83732491+airtagdev@users.noreply.github.com>
2024-02-08 19:50:40 -0700

    bug fix

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ const routes = [
   { path: '/!', file: 'settings.html' },
   { path: '/0', file: 'tabs.html' },
   { path: '/&', file: 'go.html' },
-  { path: '/w', file: 'edu.html' },
   { path: '/e', file: 'now.html' },
 ]
 
